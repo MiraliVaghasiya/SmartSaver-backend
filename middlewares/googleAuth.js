4d2812bfcc636dbae15257257d7a722f924cc396
MiraliVaghasiya <miralivaghasiya704@gmail.com>
2025-04-27 08:57:32 +0530

    api

--- middlewares/googleAuth.js
+++ middlewares/googleAuth.js
@@ -8,7 +8,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://smart-saver-backend-hv6p5zke2-miralivaghasiyas-projects.vercel.app//auth/google/callback",
+      callbackURL:
+        "https://smart-saver-backend.vercel.app/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
